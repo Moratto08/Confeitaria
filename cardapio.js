12ca80b1f1031bc9ba8f7cee6c29d81f0ed9dda4
Ramon Moratto <146308612+Moratto08@users.noreply.github.com>
2025-11-26 20:15:01 -0300

    cardapio

--- cardapio.js
+++ cardapio.js
@@ -1,1 +1,32 @@
-alert('oi foi carregado')
+//Vamos criar os elementos do HTML com JavaScript Dinamicamente
+
+function criarItemCardapio(titulo, descricao, foto){
+const divItemCardapio = document.createElement('div')
+divItemCardapio.className = 'item-cardapio'
+//colocar a classe
+
+cosnt h3Titulo = document.createElement('h3')
+h3Titulo.textContent = titulo
+
+const pDescricao = document.createElement('p')
+pDescricao.className = 'descricao'
+// adicionar a classe
+
+const img = document.createElement('img')
+img.src = foto
+img.className = 'img-item'
+
+//Adicionando os elementos na div mãe
+const divC = document.getElementById('cardapio')
+
+divItemCardapio.appendChild(h3Titulo);
+divItemCardapio.appendChild(pDescricao);
+divItemCardapio.appendChild(img);
+
+divC.appendChild(divItemCardapio)
+
+}
+
+// Execurando a função
+criarItemCardapio('Bolo de chocolate' 'Um clássico irresistível com
+            camadas de chocolate'https://www.comidaereceitas.com.br/img/sizeswp/1200x675/2020/05/bolo_chocolate_leite.jpg')
