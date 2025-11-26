c3de520a97546b3768c62757a44ed5c58db0646e
Ramon Moratto <146308612+Moratto08@users.noreply.github.com>
2025-11-26 20:27:33 -0300

    sim

--- cardapio.js
+++ cardapio.js
@@ -27,6 +27,7 @@ divC.appendChild(divItemCardapio)
 
 }
 
-// Execurando a função
-criarItemCardapio('Bolo de chocolate','Um clássico irresistível com
-            camadas de chocolate','https://www.comidaereceitas.com.br/img/sizeswp/1200x675/2020/05/bolo_chocolate_leite.jpg')
+// Executando a função
+criarItemCardapio('Bolo de chocolate', 'Um clássico irresistível com
+            camadas de chocolate',
+            'https://www.comidaereceitas.com.br/img/sizeswp/1200x675/2020/05/bolo_chocolate_leite.jpg')
