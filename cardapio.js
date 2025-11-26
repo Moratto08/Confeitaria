7bdb300bde3e4aa95927eb0ea1a22331a601596c
Ramon Moratto <146308612+Moratto08@users.noreply.github.com>
2025-11-26 20:30:48 -0300

    correção

--- cardapio.js
+++ cardapio.js
@@ -1,34 +1,29 @@
-//Vamos criar os elementos do HTML com JavaScript Dinamicamente
 
-function criarItemCardapio(titulo, descricao, foto){
-const divItemCardapio = document.createElement('div')
-divItemCardapio.className = 'item-cardapio'
-//colocar a classe
+function criarItemCardapio(titulo, descricao, foto) {
+    
+    const divItemCardapio = document.createElement('div');
+    divItemCardapio.className = 'item-cardapio';
+    // colocar a classe
+    
+    const h3Titulo = document.createElement('h3');
+    h3Titulo.textContent = titulo;
 
-const h3Titulo = document.createElement('h3')
-h3Titulo.textContent = titulo
+    const pDescicao = document.createElement('p');
+    pDescicao.textContent = descricao;
+    pDescicao.className = 'descricao';
 
-const pDescricao = document.createElement('p')
-pDescricao.textContent = descricao
-pDescricao.className = 'descricao'
-// adicionar a classe
+    const img = document.createElement('img');
+    img.src = foto;
+    img.className = 'img-item';
 
-const img = document.createElement('img')
-img.src = foto
-img.className = 'img-item'
+    const divC = document.getElementById('cardapio');
 
-//Adicionando os elementos na div mãe
-const divC = document.getElementById('cardapio')
+    divItemCardapio.appendChild(h3Titulo);
+    divItemCardapio.appendChild(pDescicao);
+    divItemCardapio.appendChild(img);
 
-divItemCardapio.appendChild(h3Titulo);
-divItemCardapio.appendChild(pDescricao);
-divItemCardapio.appendChild(img);
-
-divC.appendChild(divItemCardapio)
-
+    divC.appendChild(divItemCardapio);
 }
 
-// Executando a função
-criarItemCardapio('Bolo de chocolate', 'Um clássico irresistível com
-            camadas de chocolate',
-            'https://www.comidaereceitas.com.br/img/sizeswp/1200x675/2020/05/bolo_chocolate_leite.jpg')
+criarItemCardapio('Bolo de Chocolate', 'o padrao amado por todos',
+     'https://www.comidaereceitas.com.br/img/sizeswp/1200x675/2020/05/bolo_chocolate_leite.jpg');
